4980c1c006066138b470252267b6ea8f16c52b01
Lea Scherer <38538244+leas251@users.noreply.github.com>
2024-04-14 15:56:12 +0200

    Wall Update

---
+++ Code/ldjam55/Assets/Scripts/Scenes/GameScene/GameSceneBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSceneBehaviour : MonoBehaviour
+{
+    CreepBehaviour creepBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
