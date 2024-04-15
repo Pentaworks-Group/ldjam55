3aedaee8f8b527c72b17a4910776a66d60d5ee34
Lea Scherer <38538244+leas251@users.noreply.github.com>
2024-04-15 12:34:33 +0200

    Menu

---
+++ Code/ldjam55/Assets/Scripts/Scenes/GameModesMenu/GameModesBehaviour.cs
@@ -0,0 +1,10 @@
+using Assets.Scripts.Scenes.Menues;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModesBehaviour : BaseMenuBehaviour
+
+{
+
+}
